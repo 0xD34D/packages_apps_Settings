30bafa7900c1ee50aaff32bf6ad97e06930f3f32
0xD34D <clark@scheffsblend.com>
2012-07-24 14:14:05 -0700

    Added settings for quicknav panel behaviour.
    Change-Id: I39df9ecaca78f50b58922094ce719885996116e4

--- src/com/android/settings/UiSettings.java
+++ src/com/android/settings/UiSettings.java
@@ -39,16 +39,21 @@ public class UiSettings extends SettingsPreferenceFragment implements
     private static final String TAG = "UiSettings";
 
     private static final String KEY_STATUSBAR_TYPE = "status_bar_type";
+    private static final String KEY_AUTOHIDE_QUICKNAV = "quicknav_navbar_autohide";
+    private static final String KEY_QUICKNAV_PRESS_HIDE = "quicknav_navbar_hide_on_press";
     private static final String KEY_AUTOHIDE_NAVBAR = "sliding_navbar_autohide";
     private static final String KEY_AUTOHIDE_TIMER = "autohide_time";
 
     private static final String TYPE_SYSTEM_BAR_NORMAL = "system_bar_normal";
     private static final String TYPE_SYSTEM_BAR_SLIDER = "system_bar_slider";
     private static final String TYPE_SYSTEM_BAR_QUICKNAV = "system_bar_quicknav";
+    private static final String TYPE_SYSTEM_BAR_QUICKNAV_V2 = "system_bar_quicknav_v2";
     private static final String TYPE_SYSTEM_BAR_PHONE = "phone_statusbar";
 
 	private ListPreference mStatusbarType;
 	private CheckBoxPreference mAutoHide;
+	private CheckBoxPreference mQuicknavAutoHide;
+	private CheckBoxPreference mQuicknavPressHide;
 	private ListPreference mAutoHideTime;
 
     private final Configuration mCurConfig = new Configuration();
@@ -75,8 +80,26 @@ public class UiSettings extends SettingsPreferenceFragment implements
         mAutoHideTime = (ListPreference) findPreference(KEY_AUTOHIDE_TIMER);
         mAutoHideTime.setOnPreferenceChangeListener(this);
 
+       	mQuicknavAutoHide = (CheckBoxPreference) findPreference(KEY_AUTOHIDE_QUICKNAV);
+        mQuicknavAutoHide.setPersistent(false);
+		mQuicknavAutoHide.setChecked(Settings.System.getInt(getContentResolver(),
+								Settings.System.NAVIGATION_BAR_AUTOHIDE_QUICKNAV, 0) == 1);
+		mQuicknavAutoHide.setOnPreferenceChangeListener(this);
+
+       	mQuicknavPressHide = (CheckBoxPreference) findPreference(KEY_QUICKNAV_PRESS_HIDE);
+        mQuicknavPressHide.setPersistent(false);
+		mQuicknavPressHide.setChecked(Settings.System.getInt(getContentResolver(),
+								Settings.System.NAVIGATION_BAR_QUICKNAV_HIDE_ON_PRESS, 1) == 1);
+		mQuicknavPressHide.setOnPreferenceChangeListener(this);
+
         if (!TYPE_SYSTEM_BAR_SLIDER.equals(type)) {
             getPreferenceScreen().removePreference(mAutoHide);
+        }
+        if (!TYPE_SYSTEM_BAR_QUICKNAV.equals(type) && !TYPE_SYSTEM_BAR_QUICKNAV_V2.equals(type)) {
+            getPreferenceScreen().removePreference(mQuicknavAutoHide);
+            getPreferenceScreen().removePreference(mQuicknavPressHide);
+        }
+        if (TYPE_SYSTEM_BAR_NORMAL.equals(type) || TYPE_SYSTEM_BAR_PHONE.equals(type)) {
             getPreferenceScreen().removePreference(mAutoHideTime);
         }
     }
@@ -102,18 +125,41 @@ public class UiSettings extends SettingsPreferenceFragment implements
 			Log.d(TAG, "Statusbar type = " + value);
             if (!TYPE_SYSTEM_BAR_SLIDER.equals(value)) {
                 getPreferenceScreen().removePreference(mAutoHide);
-                getPreferenceScreen().removePreference(mAutoHideTime);
             } else {
                 getPreferenceScreen().addPreference(mAutoHide);
                 getPreferenceScreen().addPreference(mAutoHideTime);
             }
+            if (!TYPE_SYSTEM_BAR_QUICKNAV.equals(value) && !TYPE_SYSTEM_BAR_QUICKNAV_V2.equals(value)) {
+                getPreferenceScreen().removePreference(mQuicknavAutoHide);
+                getPreferenceScreen().removePreference(mQuicknavPressHide);
+            } else {
+                getPreferenceScreen().addPreference(mQuicknavPressHide);
+                getPreferenceScreen().addPreference(mQuicknavAutoHide);
+                getPreferenceScreen().addPreference(mAutoHideTime);
+            }
+            if (TYPE_SYSTEM_BAR_NORMAL.equals(value) || TYPE_SYSTEM_BAR_PHONE.equals(value))
+                getPreferenceScreen().removePreference(mAutoHideTime);
+
 		}
+
+        if (KEY_QUICKNAV_PRESS_HIDE.equals(key)) {
+			boolean value = ((Boolean) objValue.equals(Boolean.TRUE));
+			Settings.System.putInt(getContentResolver(), Settings.System.NAVIGATION_BAR_QUICKNAV_HIDE_ON_PRESS,
+					value ? 1 : 0);
+        }
+
+        if (KEY_AUTOHIDE_QUICKNAV.equals(key)) {
+			boolean value = ((Boolean) objValue.equals(Boolean.TRUE));
+			Settings.System.putInt(getContentResolver(), Settings.System.NAVIGATION_BAR_AUTOHIDE_QUICKNAV,
+					value ? 1 : 0);
+            //mAutoHideTime.setEnabled(value);
+        }
 
         if (KEY_AUTOHIDE_NAVBAR.equals(key)) {
 			boolean value = ((Boolean) objValue.equals(Boolean.TRUE));
 			Settings.System.putInt(getContentResolver(), Settings.System.NAVIGATION_BAR_AUTOHIDE_SLIDER,
 					value ? 1 : 0);
-            mAutoHideTime.setEnabled(value);
+            //mAutoHideTime.setEnabled(value);
         }
 
         if (KEY_AUTOHIDE_TIMER.equals(key)) {
